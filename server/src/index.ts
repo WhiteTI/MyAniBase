000660159a9afe216fd3a8cc8816a169ce06d63a
white <grigsgt@gmail.com>
2025-07-26 21:06:49 +0300

    Add cors and swagger plagin

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(cors())
+    .use(swagger())
+    .get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
